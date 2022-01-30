74e825f1ff2dc19553f3d2d9d9b404f3fb7398d0
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-01-30 10:14:26 -0800

    Update main.c

--- main.c
+++ main.c
@@ -8,12 +8,14 @@
 
 
 /*Function Prototypes*/
-
 void printPrompt();
 void executeCommand();
 void changeDirectory();
 void tokenize();
 int countArgs();
+void addBackground();
+void add();
+
 
 int main() {
 	while(1) {
@@ -23,18 +25,58 @@ int main() {
 		tokenize(tokens, readline(holdPrompt));
 		if (!strcmp(tokens[0], "cd")) {
 			changeDirectory(tokens);
+		} else if (!strcmp(tokens[0], "bg")) {
+			addBackground(tokens);
+		} else if (!strcmp (tokens[0], "exit")) {
+			exit(1);
 		}
 		executeCommand(tokens);
 	}
 	return 0;
 }
 
+int listLength = 0;
+
+typedef struct Node {
+	char command[1024];
+	pid_t pid;
+	struct Node* next;
+} node;
+
+node* head = NULL;
+
+void addBackground(char** commands) {
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		execvp(commands[1], commands + 1);
+	} else if (pid > 0) {
+		add(commands, pid);
+	}
+}
+
+
+void add(char** command, pid_t pid) {
+	node* newNode = (node*)malloc(sizeof(node));
+	newNode -> pid = pid;
+	newNode -> command[0] = '\0';
+	int i = 1;
+
+	while (command[i] != NULL) {
+		strcat(newNode -> command, command[i]);
+		strcat(newNode -> command, " ");
+		i++;
+	}
+	newNode -> next = head;
+	head = newNode;
+	listLength++;
+}
+
 int countArgs(char** tokens) {
-	int i = 0;
-	while (tokens[i] != NULL){
+	int i = 0;;
+	while (tokens[i] != NULL) {
 		tokens[i++];
 	}
-	printf("%d", i);
 	return i;
 }
 
