20b4cbcc3c79dea631022104340aaa6057ea253d
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-02-10 17:27:36 -0800

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 
 /*Function Prototypes*/
-void printPrompt();
+void displayPrompt();
 void executeCmd();
 void changeDirectory();
 void tokenize();
@@ -19,31 +20,34 @@ void listNodes();
 void checkChildTermination();
 void runCommands();
 void freeList();
-
+void createNode();
 
 int main() {
 	while(1) {
-		char holdPrompt[2024];
-		printPrompt(holdPrompt);
-		char* tokens[128];
-		char* input = readline(holdPrompt);
+		char holdPrompt[256];
+		displayPrompt(holdPrompt);
+		char* tokens[256];
+		char* user_input = readline(holdPrompt);
 
-		if(countArgs(input) < 1) {
-			free(input);
+		if(countArgs(user_input) < 1) {
+			free(user_input);
 			continue;
 		}
 
 		checkChildTermination();
 
 
-		tokenize(tokens, input);
+		tokenize(tokens, user_input);
 		runCommands(tokens);
+		free(user_input);
 	}
 	freeList();
 	return 0;
 }
 
-/* typdef sturcture used to group tgether data in a node datatype. */
+/* Purpose:	typdef sturcture used to group tgether data in a node datatype. 
+ * Source:	CSC 360 tutorial-2 slides.
+ */
 typedef struct Node {
 	pid_t pid;
 	char command[1024];
@@ -52,20 +56,23 @@ typedef struct Node {
 
 node* head = NULL;
 
+/* Purpose: 	Execute the various supported commands that the user can run.
+ * Parameters:	char** cmd - the command inputted by the user.
+ * Returns:	void
+ */
+void runCommands(char** cmd) {
 
-void runCommands(char** tokens) {
-
-	if (tokens[0] != NULL){
-		if (strcmp(tokens[0], "cd") == 0) {
-			changeDirectory(tokens);
-		}else if (strcmp(tokens[0], "bg") == 0) {
-			add(tokens);
-		}else if (strcmp(tokens[0], "bglist") == 0) {
+	if (cmd[0] != NULL){
+		if (strcmp(cmd[0], "cd") == 0) {
+			changeDirectory(cmd);
+		}else if (strcmp(cmd[0], "bg") == 0) {
+			add(cmd);
+		}else if (strcmp(cmd[0], "bglist") == 0) {
 			listNodes();
-		}else if (strcmp(tokens[0], "exit") == 0) {
+		}else if (strcmp(cmd[0], "exit") == 0) {
 			exit(1);
 		}else{
-			executeCmd(tokens);
+			executeCmd(cmd);
 		}
 	}
 }
@@ -118,14 +125,13 @@ int countArgs(char** tokens) {
  * Source:	CSC 360 tutorial-2 slides. 	
  */
 
-void tokenize(char** tokens, char* userInput) {
-	char* args = strtok(userInput, " ");
+void tokenize(char** argv, char* userInput) {
+	argv[0] = strtok(userInput, " ");
 	int i = 0;
-	while (args) {
-		tokens[i++] = args;
-		args = strtok(NULL, " ");
+	for (i; argv[i] != NULL; i++) {
+		argv[i+1] = strtok(NULL, " ");
 	}
-	tokens[i] = NULL;
+	argv[i] = NULL;
 }
 
 
@@ -137,14 +143,12 @@ void tokenize(char** tokens, char* userInput) {
  * Parameters: 	char* holdPrompt - 
  * Returns:	void	
  */
-void printPrompt(char* holdPrompt) {
+void displayPrompt(char* holdPrompt) {
 	holdPrompt[0] = '\0';
 
-	char hostname[256];
-	gethostname(hostname, sizeof(hostname));
+	char hostname[256], curr_dir[256];
 
-	
-	char curr_dir[256];
+	gethostname(hostname, sizeof(hostname));
 	getcwd(curr_dir, sizeof(curr_dir));
 
 	snprintf(holdPrompt, 1024, "%s@%s: %s > ", getlogin(), hostname, curr_dir);
@@ -186,23 +190,22 @@ void executeCmd(char** cmd) {
 /*----------Part 3----------*/
 
 
-/* Purpose: 	Check if the child process terminates
- * Parameters: 	
- * Returns:
- * Source: 	This function is follows the pseudo implementation learned in tutorial-3
+/* Purpose: 	Check if the child process terminates.
+ * Parameters:	void 	
+ * Returns:	void
+ * Source: 	This function is follows the pseudo implementation learned in tutorial-3.
  */
 
 void checkChildTermination(void) {
 	if (determineListLength() > 0) {
-		pid_t pid = waitpid(0, NULL, WNOHANG);
-
-		while (pid > 0) {
+		pid_t ter = waitpid(0, NULL, WNOHANG);
+		while (ter > 0) {
 			node* t_head = head;
-			if (head -> pid == pid) {
+			if (head -> pid == ter) {
 				printf("%d: %s has terminated.\n", t_head -> pid, t_head -> command);
 				head = head -> next;
 			} else {
-				while (t_head -> next -> pid != pid) {
+				while (t_head -> next -> pid != ter) {
 					t_head = t_head -> next;
 				}
 				printf("%d: %s has terminated.\n", t_head -> next -> pid, 
@@ -210,11 +213,15 @@ void checkChildTermination(void) {
 				t_head -> next = t_head -> next -> next;
 				free(t_head -> next);
 			}
-			pid = waitpid(0, NULL, WNOHANG);
+			ter = waitpid(0, NULL, WNOHANG);
 		}
 	}
 }
 
+/* Purpose:	Display the currently running background processes.
+ * Parameters:	void
+ * Returns:	void
+ */
 void listNodes(void) {
 	for (node* t_head = head; t_head != NULL; t_head = t_head -> next){
 		printf("%d: %s\n", t_head -> pid, t_head -> command);
@@ -222,19 +229,29 @@ void listNodes(void) {
 	printf("Total Background Jobs: %d\n", determineListLength());
 }	
 
+/* Purpose: 	Add a new node (command) to the front of the linked list.	
+ * Parameters: 	char** cmd - the command.
+ * Returns:	void
+ */
 void add(char** cmd) {
 	pid_t pid = fork();
-	if (!pid) {
+/*	if (!pid) {
 		execvp(cmd[1], cmd + 1);
 	} else {
-		node* newNode = (node*)malloc(sizeof(node));
-		newNode -> pid = pid;
-		newNode -> command[0] = '\0';
-		for (int i = 1; cmd[i] != NULL; i++) {
-			strcat(newNode -> command, cmd[i]);
-			strcat(newNode -> command, " ");
-		}
-		newNode -> next = head;
-		head = newNode;
-	}
+		createNode(cmd, pid);
+	}*/
+	pid == 0 ? execvp(cmd[1], cmd + 1) : createNode(cmd, pid);
+
 }
+
+void createNode(char** cmd, pid_t pid) {
+	node* new_node = (node*)malloc(sizeof(node));
+	new_node -> command[0] = '\0';
+	new_node -> pid = pid;
+	for (int i = 1; cmd[i] != NULL; i++) {
+		strcat(new_node -> command, cmd[i]);
+		strcat(new_node -> command, " ");
+	}
+	new_node -> next = head;
+	head = new_node;
+}	
