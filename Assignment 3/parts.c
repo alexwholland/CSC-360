70a262a88d5d433a9506e70ff84302263e2da265
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-03-25 10:59:01 -0700

    Updated Part 2 and implemented Part 3
    Part 3 has been briefly tested but seems to be working.

--- Assignment 3/parts.c
+++ Assignment 3/parts.c
@@ -65,15 +65,17 @@ int StartPlusEnd();
 void disklist();
 void printDirectory();
 
+int diskget();
+
 int main(int argc, char* argv[]) {
 	/*Logic implemented from Tutorial 8 slides*/
     	#if defined(PART1)
         	diskinfo(argc, argv);
     #elif defined(PART2)
         disklist(argc, argv);
-/*    #elif defined(PART3)
+    #elif defined(PART3)
         diskget(argc, argv);
-    #elif defined(PART4)
+/*    #elif defined(PART4)
         diskput(argc, argv);
     #elif defined(PART5)
         diskfix(argc, argv);
@@ -180,12 +182,18 @@ void disklist(int argc, char** argv) {
     }
     
     // Open and assemble the disk image
-    int fd = open(argv[1], O_RDWR);
-    struct stat fileStats;
+    	int fd = open(argv[1], O_RDWR);
+	if (fd < 0) {
+		printf("Error reading disk image\n");
+	}
 
-    fstat(fd, &fileStats);
-
-    char* data = mmap(NULL, fileStats.st_size, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0);
+    struct stat buffer;
+    fstat(fd, &buffer);
+    char* data = mmap(NULL, buffer.st_size, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0);
+    if (data == MAP_FAILED) {
+	printf("Colud not mmap\n");
+	exit(1);
+    }
     struct superblock_t* superBlock = (struct superblock_t*) data;
 
     int blockSize, rootStart = 0;
@@ -194,28 +202,19 @@ void disklist(int argc, char** argv) {
 
     struct dir_entry_t* root_block;
 
-    int offSet = rootStart;
     int curr = 0;
-    if (argc == 3 && strcmp(dirName, "/")) {
-        while(1) {
-            for (int i = offSet; i < offSet + blockSize; i += 64) {
-                root_block = (struct dir_entry_t*) (data+i);
-                const char* name = (const char*)root_block->filename;
-                if (!strcmp(name, tokens[curr])) {
-                    curr++;
-                    offSet = ntohl(root_block->starting_block) * blockSize;
-                    if (curr == dirs || !strcmp(dirName, "/")) {
-                        for (int j = offSet; j < offSet + blockSize; j += 64) {
-                            root_block = (struct dir_entry_t*) (data + j);
-                            if (ntohl(root_block->size) == 0) continue;
-                          	printDirectory(root_block);
-                        }
-                        return;
-                    }
-                    break;
-                }
-            }
-        }
+    	if (argc == 3 && strcmp(dirName, "/")) {
+		for (int i = 0; i < dirs; i++) {
+            		for (int j = rootStart; j < rootStart + blockSize; j += 64) {
+             			root_block = (struct dir_entry_t*) (data+j);
+                		const char* name = (const char*)root_block->filename;
+                		if (!strcmp(name, tokens[curr])) {
+                    			curr++;
+                    			rootStart = ntohl(root_block->starting_block) * blockSize;
+                                	break;
+                		}
+     			}	
+		}
    }
     
     if (argc == 2 || curr == dirs || !strcmp(dirName, "/")) {
@@ -226,8 +225,6 @@ void disklist(int argc, char** argv) {
         }
     }    
 }
-
-
 
 
 void printDirectory(struct dir_entry_t* root_block) {
@@ -241,4 +238,126 @@ void printDirectory(struct dir_entry_t* root_block) {
 		root_block->modify_time.hour,
 		root_block->modify_time.minute,
 		root_block->modify_time.second); 
+}
+
+
+struct superblock_t sb;
+/*Part 3*/
+
+int diskget(int argc, char* argv[]) {
+	char* directory = argv[2];
+	char* tokens[128];
+	char* args = strtok(directory, "/");
+	int i = 0;
+	while (args) {
+		tokens[i++] = args;
+		args = strtok(NULL, "/");
+	}
+	tokens[i] = NULL;
+//	printf("%s", tokens[0]);
+//	printf("%s", tokens[1]);
+
+if (argc != 4) {
+        printf("Error: Incorrect usage. Needs 4 arguments\n");
+        return 0;
+    }
+    char *file_name;
+    char *file_placement;
+    file_name = tokens[i-1];
+    file_placement = argv[3];
+
+    int fd = open(argv[1], O_RDWR);
+    struct stat buffer;
+    if (fstat(fd, &buffer) < 0) {
+        perror("FSTAT ERROR");
+        return 1;
+    }
+    char *address = mmap(NULL, buffer.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    int block_size = 0, root_start = 0, root_blocks = 0, fat_start = 0, fat_blocks = 0;
+    memcpy(&block_size, address + 8, 2);
+    block_size = htons(block_size);
+    memcpy(&root_start, address + 24, 2);
+    root_start = htons(root_start);
+    memcpy(&root_blocks, address + 28, 2);
+    root_blocks = htons(root_blocks);
+    memcpy(&fat_start, address + 16, 2);
+    fat_start = htons(fat_start);
+    memcpy(&fat_blocks, address + 20, 2);
+    fat_blocks = htons(fat_blocks);
+
+    int root_block = root_start * block_size;
+
+    while(1){
+        int i;
+        for (i = root_block; i < root_block + block_size; i += 64) {
+           int curr = 0;
+           memcpy(&curr, address + i, 1);
+
+           // if we found a file entry
+           if (curr == 3) {
+               char name[31];
+               memcpy(&name, address + i + 27, 31);
+
+               // compare names, if the same, the file has been found
+               if (!strcmp(name, file_name)) {
+                   FILE *fp;
+                   fp = fopen(file_placement, "wb");
+
+                   int current_block = 0, num_blocks = 0, fat_address = 0;
+
+                   // find how many blocks it needs
+                   memcpy(&num_blocks, address + i + 5, 4);
+                   num_blocks = htonl(num_blocks);
+
+                   // printf("Current block address: %x\n", i);
+                   memcpy(&current_block, address + i + 1, 4);
+                   current_block = htonl(current_block);
+
+                   int file_size;
+                   memcpy(&file_size, address+i+9, 4);
+                   file_size = htonl(file_size);
+                   int j;
+                   // for each needed block -1, copy block contents in block_size intervals
+                   for (j = 0; j < num_blocks-1; j++) {
+                       fwrite(address + (block_size * current_block), 1, block_size, fp);
+                       fat_address = fat_start * block_size + current_block * 4;
+
+                       memcpy(&curr, address + fat_address, 4);
+                       curr = htonl(curr);
+                       current_block = curr;
+                   }
+
+                   // determine remaining size left to copy
+                   int remaining = file_size % block_size;
+
+                   // if the remaining is 0, then it's a full block_size multiple
+                   if (remaining == 0) {
+                      // to copy the last contents
+                      remaining = block_size;
+                   }
+
+                   // write remaining information
+                   fwrite(address + (block_size * current_block), 1, remaining, fp);
+                   fat_address = fat_start * block_size + current_block * 4;
+                   memcpy(&curr, address + fat_address, 4);
+
+                   munmap(address, buffer.st_size);
+                   close(fd);
+                   printf("Getting %s from %s and writing as %s.\n", file_name, argv[1], file_placement);
+                   return 0;
+               }
+           }
+        }
+        int fat_dir_location = (root_block/block_size)*4 + (fat_start * block_size);
+        memcpy(&root_block, address + fat_dir_location, 4);
+        root_block = htonl(root_block);
+        if (root_block == -1){ break; }
+        root_block = root_block * block_size;
+    }
+
+    printf("File not found.\n");
+    munmap(address, buffer.st_size);
+    close(fd);
+    return 0;	
 }
